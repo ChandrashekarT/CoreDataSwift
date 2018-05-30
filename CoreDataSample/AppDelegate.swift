f9f7413361310a35350ab354c02e2977003d26ec
Chandrashekar <chandrashekar203@gmail.com>
2018-05-30 16:54:11 +0530

    Remove Comment --Chandu

--- CoreDataSample/AppDelegate.swift
+++ CoreDataSample/AppDelegate.swift
@@ -18,7 +18,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         
-        //Comment 30 May -- Chandu
         return true
     }
 
